e420c90ab1b93ff7dc92203a35f9fb79b5634c01
azennto <azenntoyuchi@gmail.com>
2019-12-11 23:33:04 +0900

    Revert "change ui"
    This reverts commit 131683acef6401fdb5efc3e7dc109102544e4e09.

--- atcoder_create_filename.js
+++ atcoder_create_filename.js
@@ -8,38 +8,44 @@
 // @namespace    https://github.com/azenntoyuchi
 // ==/UserScript==
 
+
+
 //インターフェースの作成
-(() => {
-  const element_main = document.getElementsByClassName('col-sm-12');
-  const filename_str= `${location.href.split('/')[6]}.cpp`;
-  const style = document.createElement('style');
-  style.setAttribute('type', 'text/css');
+(function() {
+  var element_main = document.getElementsByClassName("col-sm-12");
+  var url = location.href;
+  var filename_str= "'"+ url.split("/")[6] + ".cpp'";
+
+  //ボタンのCSSをheadにぶちこむ
+  var style = document.createElement("style");
+  style.setAttribute("type", "text/css");
   style.textContent =
-    `#create_filename_button {
-       padding: 10px 20px;
-       position: absolute;
-       left: 87%;
-       top: 60px;
-     }`;
+    "#create_filename_button{\n"+
+      "position: absolute;\n"+
+      "left: 87%;\n"+
+      "padding: 10px 20px;\n"+
+    "}";
   document.head.appendChild(style);
 
-  const script = document.createElement('script');
-  script.setAttribute('type', 'text/javascript');
+  //jsをheadにぶちこむ
+  var script = document.createElement("script");
+  script.setAttribute("type", "text/javascript");
   script.textContent =
-    `const copy_filename = str => {
-       const copyFrom = document.createElement('textarea');
-       copyFrom.textContent = str;
-       const bodyElm = document.getElementsByTagName('body')[0];
-       bodyElm.appendChild(copyFrom);
-       copyFrom.select();
-       document.execCommand('copy');
-       bodyElm.removeChild(copyFrom);
-    }`;
+    "function copy_filename(str){\n"+
+      "var copyFrom = document.createElement('textarea');\n"+
+      "copyFrom.textContent = str;\n"+
+      "var bodyElm = document.getElementsByTagName('body')[0];\n"+
+      "bodyElm.appendChild(copyFrom);\n"+
+      "copyFrom.select();\n"+
+      "document.execCommand('copy');\n"+
+      "bodyElm.removeChild(copyFrom);\n"+
+    "}";
   document.head.appendChild(script);
 
-  var button = document.createElement('button');
-  button.setAttribute('id', 'create_filename_button');
-  button.setAttribute('onclick', `copy_filename(${filename_str})`);
-  button.textContent = 'filename copy';
+  //ボタンおしたらfilename_strをクリップボードに保存したい
+  var button = document.createElement("button");
+  button.setAttribute("id", "create_filename_button");
+  button.setAttribute("onclick","copy_filename("+filename_str+")");
+  button.textContent = "filename copy";
   element_main[1].insertBefore(button,element_main[1].firstChild);
 })();
