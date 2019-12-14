f96d11b0e6bec71d5cb0b4ced33391f20e241720
azennto <azenntoyuchi@gmail.com>
2019-12-14 16:39:11 +0900

    clickした時のUIを追加
    ガチプロ	midori56green 氏の†力†によって私のプロダクトに†禁断のUI†が付与された。。。

--- atcoder_create_filename.js
+++ atcoder_create_filename.js
@@ -16,18 +16,42 @@
   const style = document.createElement('style');
   style.setAttribute('type', 'text/css');
   style.textContent =
-    `#create_filename_button {
+    `
+    #create_filename_button {
        padding: 10px 20px;
        position: absolute;
        left: 87.2%;
        top: 60px;
-     }`;
+     }
+
+     #popup{
+       display: none;
+       position: absolute;
+       left: 90%;
+       top: 110px;
+       padding: 10px 20px;
+       background: #ccc;
+       border-radius: 5px;
+     }
+     `;
   document.head.appendChild(style);
+
+  var div = document.createElement('div');
+  div.setAttribute('id', 'popup');
+  div.textContent = 'click!';
+  element_main[1].insertBefore(div,element_main[1].firstChild);
+
+  var button = document.createElement('button');
+  button.setAttribute('id', 'create_filename_button');
+  button.setAttribute('onclick', `copy_filename('${filename_str}')`);
+  button.textContent = 'filename copy';
+  element_main[1].insertBefore(button,element_main[1].firstChild);
+
 
   const script = document.createElement('script');
   script.setAttribute('type', 'text/javascript');
   script.textContent =
-    `const copy_filename = str => {
+      `const copy_filename = str => {
        const copyFrom = document.createElement('textarea');
        copyFrom.textContent = str;
        const bodyElm = document.getElementsByTagName('body')[0];
@@ -35,12 +59,27 @@
        copyFrom.select();
        document.execCommand('copy');
        bodyElm.removeChild(copyFrom);
-    }`;
+    }
+    `;
   document.head.appendChild(script);
 
-  var button = document.createElement('button');
-  button.setAttribute('id', 'create_filename_button');
-  button.setAttribute('onclick', `copy_filename('${filename_str}')`);
-  button.textContent = 'filename copy';
-  element_main[1].insertBefore(button,element_main[1].firstChild);
+  const jquery = document.createElement('script');
+  jquery.setAttribute('type', 'text/javascript');
+  jquery.textContent =
+      `
+      $('#create_filename_button').on('click',function(){
+        $('#popup').fadeIn(300);
+        var i=0;
+        const scond_count = setInterval(function(){
+          i++;
+          if(i <= 2){
+            $('#popup').fadeOut();
+            clearInterval(scond_count);
+          }
+          console.log(i);
+        },1000);
+      });
+    `;
+  document.head.appendChild(jquery);
+
 })();
